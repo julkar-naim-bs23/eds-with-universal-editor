08b13b12779e922576f8043491d21366bfde9060
Julkar Naim <julkar.naim.cse@gmail.com>
2025-04-15 20:03:03 +0600

    banner block

---
+++ blocks/banner/banner.js
@@ -0,0 +1,6 @@
+// import { moveInstrumentation } from '../../scripts/scripts';
+// import { createOptimizedPicture } from '../../scripts/aem';
+
+export default function decorate(block) {
+  block.textContent = '';
+}
